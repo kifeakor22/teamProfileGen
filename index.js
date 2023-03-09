bbb7d07ae50fd3a177d9f819c459ea1356599ada
Kc <kifeakor@yahoo.com>
2023-03-09 10:51:35 +0000

    completed code to add team memeber functions

--- index.js
+++ index.js
@@ -13,3 +13,130 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const employees = [];  // an empty array  to hold employees 
+
+function addIntern() {
+    return inquirer.prompt([
+         {
+            type: "input",
+            name: "internName",
+            message: "what is your Intern's Name"
+        }, 
+        {
+            type: "input",
+            name: "internId",
+            message: "what is your Interns's ID"
+            
+        }, 
+        {
+            type: "input",
+            name: "internEmail",
+            message: "what is your intern's Email"
+        }, 
+        {
+            type: "input",
+            name: "internSchool",
+            message: "what is your Intern's School?"
+        }
+    
+
+    ]).then(answers => {
+        const intern = new Intern (answers.internName,answers.internId, answers.internEmail,answers.internSchool)
+        employees.push(intern)
+        console.log(intern)
+        creatTeam()
+    })
+}
+
+function addEngineer () {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "what is your Engineer's Name"
+        }, 
+        {
+            type: "input",
+            name: "engineerId",
+            message: "what is your Engineer's ID"
+            
+        }, 
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "what is your Engineer's Email"
+        }, 
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "what is your Engineer's Github"
+        }
+    
+
+    ]).then(answers => {
+        const engineer = new Engineer (answers.engineerName,answers.engineerId, answers.engineerEmail,answers.engineerGithub)
+        employees.push(engineer)
+        console.log(engineer)
+        creatTeam()
+    })
+}
+
+function promptManager() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "Enter the team manager's name:",
+    },
+    {
+      type: "input",
+      name: "managerId",
+      message: "Enter the team manager's employee ID:",
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "Enter the team manager's email address:",
+    },
+    {
+      type: "input",
+      name: "managerofficeNumber",
+      message: "Enter the team manager's office number:",
+    },
+  ]).then(answers => {
+    const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerofficeNumber)
+    employees.push(manager)
+    console.log(manager)
+    creatTeam()
+  });
+
+}
+
+promptManager()
+
+function creatTeam () {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "memberRole",
+            message: "What is your role?",
+            choices: [
+                "Engineer",
+               "Intern",
+               "Not applicable"
+            ]
+        }
+    ]).then(userRole => {
+        if(userRole.memberRole === "Engineer"){
+            addEngineer()
+
+        }else if (userRole.memberRole === "Intern"){
+            addIntern()
+
+        }else {
+            buildTeam()
+
+        }
+    })
+
+}
